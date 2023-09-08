6dbbf1bb922a27f691f4494990faa9a425d5173e
KevenAbraham <abraham.keven20@gmail.com>
2023-09-08 10:13:39 -0300

    meu primieiro commit

---
+++ exec2/Program.cs
@@ -0,0 +1,18 @@
+﻿
+namespace exec2 {
+    internal class Program {
+        public static void Main(string[] args) {
+            int A, B, X;
+
+            Console.WriteLine("Informe o primeiro valor da soma: ");
+            A = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o segundo valor da soma: ");
+            B = int.Parse(Console.ReadLine());
+
+            X = A + B;
+
+            Console.WriteLine("O resultado entre: " + A + " + " + B + " é igual a: " + X);
+        }
+    }
+}
