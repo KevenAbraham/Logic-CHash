7eca8905ea3db17bfb998ad58121f5cd3740a372
KevenAbraham <abraham.keven20@gmail.com>
2023-09-08 10:22:18 -0300

    exercicio 3 - calculo de circunferência

---
+++ exec3/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace exec3 {
+    internal class Program {
+        public static void Main(string[] args) {
+            double n = 3.14159, A, R;
+       
+            Console.WriteLine("Informe o valor de R:");
+            R = double.Parse(Console.ReadLine());
+        
+            A = Math.Pow(R, 2) * n;
+        
+            Console.WriteLine("A = " + A);
+        }
+    }
+}
