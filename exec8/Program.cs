9ee06f69afff9f41685d048b4b3f8c0e89fa4541
KevenAbraham <abraham.keven20@gmail.com>
2023-09-08 11:04:24 -0300

    exercicio 08 - diferneça entre a multiplicação entre 4 valores

---
+++ exec8/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace exec8 {
+    internal class Program {
+        public static void Main(string[] args) {
+            int a, b, c, d, diff;
+
+            Console.WriteLine("Informe o primeiro valor a ser lido pelo console: ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o primeiro valor a ser lido pelo console: ");
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o primeiro valor a ser lido pelo console: ");
+            c = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o primeiro valor a ser lido pelo console: ");
+            d = int.Parse(Console.ReadLine());
+
+            diff = (a * b - c * d);
+
+            Console.WriteLine("A diferença entre A * B - C * D é: " + diff);
+        }
+    }
+}
