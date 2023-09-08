b1f42af75e64a3ef72c5452d3a17b691f1b5e151
KevenAbraham <abraham.keven20@gmail.com>
2023-09-08 10:27:13 -0300

    exercicio 4 - soma de valores

---
+++ exec4/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace exec4 {
+    internal class Program {
+        public static void Main(string[] args) {
+            int a, b, soma;
+
+            Console.WriteLine("Informe o primeiro valor a ser somado: ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o segundo valor a ser somado: ");
+            b = int.Parse(Console.ReadLine());
+
+            soma = a + b;
+            
+            Console.WriteLine("A soma entre os valores " + a + " e " + b + " é igual à: " + soma);
+        }
+    }
+}
