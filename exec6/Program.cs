d43ef7df0c26656da8b1439786e4d70fac9a40f8
KevenAbraham <abraham.keven20@gmail.com>
2023-09-08 10:52:22 -0300

    exercicio 06 - media das notas dos alunos

---
+++ exec6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+namespace exec6 {
+    internal class Program {
+        public static void Main(string[] args) {
+            double p1, p2, media; 
+
+            Console.WriteLine("Informe a primeira nota: ");
+            p1 = double.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Informe a segunda nota: ");
+            p2 = double.Parse(Console.ReadLine());
+
+            media = (p1 + p2) / 2;
+
+            Console.WriteLine("A média entre as notas é igual a: " + media);
+        }
+    }
+} 
