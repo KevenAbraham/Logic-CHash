5025a0221350e7d497d4a5379f700c7ba4722792
KevenAbraham <abraham.keven20@gmail.com>
2023-09-08 21:37:48 -0300

    exercicio 15 - média de combustível em uma viagem

---
+++ exec15/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace exec15 {
+    internal class Program {
+        public static void Main(string[] args) {
+            Console.WriteLine("Informe a duração da viagem em horas:");
+            int hour = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe a média de Litros por Km: ");
+            int liter = int.Parse(Console.ReadLine());
+
+            double distance = hour * liter;
+
+            double avgLiter = distance / 12.0;
+
+            Console.WriteLine($"Média de litros nessa viagem é de: {avgLiter:F3} km/L.");
+        }
+    }
+}
