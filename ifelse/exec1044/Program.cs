1c0b860a806da7f36c550b852af6842f8d067d57
KEVEN ABRAHAM SOUZA <ksouza@websupply.com.br>
2023-09-11 11:24:39 -0300

    exercicio 19 - multiplos

---
+++ ifelse/exec1044/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace exec1044 {
+    internal class Program {
+        public static void Main(string[] args) {
+            Console.WriteLine("\n=============================================");
+            Console.WriteLine("         Leitor de Valores Múltiplos         ");
+            Console.WriteLine("=============================================");
+
+            Console.WriteLine("\nInforme um valor e outro para conferir se são múltiplo.");
+            string[] vet = Console.ReadLine().Split(' ');
+
+            int a = int.Parse(vet[0]);
+            int b = int.Parse(vet[1]);
+
+            int multiplo;
+
+            if (b % a == 0) {
+                Console.WriteLine("São múltiplos.");
+            } else {
+                Console.WriteLine("Não são múltiplos.");
+            }
+        }
+    }
+}
