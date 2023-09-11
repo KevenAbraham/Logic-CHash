eac0ad7cccad82f89357f456dca8b5b1694d2a69
KEVEN ABRAHAM SOUZA <ksouza@websupply.com.br>
2023-09-11 12:18:56 -0300

    exercicio 21 - calculo de reajuste salarial.

---
+++ ifelse/exec1048/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Globalization;
+
+namespace exec21 {
+    internal class Program {
+        public static void Main(string[] args) {
+            double newSalary, increase, salary;
+            int percent;
+
+            Console.WriteLine("=====================================================");
+            Console.WriteLine("                 REAJUSTE DE SALÁRIO                 ");
+            Console.WriteLine("=====================================================");
+
+            Console.WriteLine("Informe o seu salário atual:");
+            salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if (salary <= 400.00) {
+                increase = salary * 0.15;
+                newSalary = salary + increase;
+                percent = 15;
+            } else if (salary <= 800.00) {
+                increase = salary * 0.12;
+                newSalary = salary + (salary * 0.12);
+                percent = 12;
+            } else if (salary <= 1200.00) {
+                increase = salary * 0.10;
+                newSalary = salary + (salary * 0.10);
+                percent = 10;
+            } else if (salary <= 2000.00) {
+                increase = salary * 0.07;
+                newSalary = salary + (salary * 0.07);
+                percent = 7;
+            } else {
+                increase = salary * 0.04;
+                newSalary = salary + (salary * 0.04);
+                percent = 4;
+            }
+
+            Console.WriteLine($"Novo salário: R${newSalary:F2}\nReajuste ganho: R${increase:F2}\nEm percentual: {percent}%");
+
+
+        }
+    }
+}
