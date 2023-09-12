45568209512fcff8a816be79f389a5dc0aa55e0b
KEVEN ABRAHAM SOUZA <ksouza@websupply.com.br>
2023-09-12 09:03:54 -0300

    exercicio 1037

---
+++ ifelse/exec1037/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace exec1037 {
+    internal class Program {
+        public static void Main(string[] args) {
+            Console.WriteLine("===========================================");
+            Console.WriteLine("      INFORME UM VALOR DE 0 A 100          ");
+            Console.WriteLine("===========================================");
+
+            double num = double.Parse(Console.ReadLine());
+
+            if (num <= 25.0) {
+                Console.WriteLine("Intervalo [0,25]");
+            } else if (num <= 50.0) {
+                Console.WriteLine("Intervalo [25,50]");
+            } else if (num <= 75.0) {
+                Console.WriteLine("Intervalo [50,75]");
+            } else if (num <= 100.0 ) {
+                Console.WriteLine("Intervalo [75,100]");
+            } else {
+                Console.WriteLine("Fora de Intervalo");
+            }
+
+        }
+    }
+}
