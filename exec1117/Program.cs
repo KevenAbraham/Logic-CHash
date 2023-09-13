02e7dba4b6bed24387a4312afa436d6f3464dff7
KevenAbraham <abraham.keven20@gmail.com>
2023-09-13 12:49:20 -0300

    exericcio a ser finalizado

---
+++ exec1117/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace exec1117 {
+    internal class Program {
+        static void Main(string[] args) {
+            double n1, n2;
+
+            Console.WriteLine("========================================");
+            Console.WriteLine("             NOTAS DOS ALUNOS           ");
+            Console.WriteLine("========================================");
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            n1 = double.Parse(Console.ReadLine());
+            n2 = double.Parse(Console.ReadLine());
+
+            
+        }
+    }
+}
