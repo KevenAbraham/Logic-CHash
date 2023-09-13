5a160352309e0eca1101038b798b951da48bc7aa
KevenAbraham <abraham.keven20@gmail.com>
2023-09-13 10:55:12 -0300

    exercicio 1114 - senha

---
+++ exec1114/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace exec1114 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("===================================");
+            Console.WriteLine("          LEITOR DE SENHA          ");
+            Console.WriteLine("===================================");
+
+            Console.WriteLine("Informe a sua senha:");
+            int password = int.Parse(Console.ReadLine());
+
+            while (password != 2002) {
+                Console.WriteLine("Senha Inválida!");
+                Console.WriteLine("\nInforme a sua senha:");
+                password = int.Parse(Console.ReadLine());
+            } 
+
+            Console.WriteLine("Acesso permitido!");
+        }
+    }
+}
