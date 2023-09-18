e101e826abc6d94456f64b7589d70c1276cdf408
KevenAbraham <abraham.keven20@gmail.com>
2023-09-18 10:19:09 -0300

    enviando exercicio 1072

--- exec1067/Program.cs
+++ exec1067/Program.cs
@@ -8,7 +8,7 @@
 
             for (int i = 1; i <= num; i++) {
                 if (i % 2 != 0) {
-                    Console.Write(i + " ");
+                    Console.WriteLine(i + " ");
                 }
             }
         }
