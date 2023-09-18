086e0daf4ade1ab2d4d2d53adc687e2278afedac
KevenAbraham <abraham.keven20@gmail.com>
2023-09-18 09:38:14 -0300

    enviando o exemplo de laço do while

---
+++ ExemploFacaEnquanto/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ExemploFacaEnquanto {
+    internal class Program {
+        public static void Main(string[] args) {
+            double c, f;
+            char repetir;
+
+            do {
+                Console.WriteLine("Digite a temperatura em Clesius:");
+                c = double.Parse(Console.ReadLine());
+                f = 9.0 * c / 5.0 + 32.0;
+                Console.WriteLine("Equivalente em Fahrenheit: " + f.ToString("F1"));
+                Console.WriteLine("Deseja repetir? (s/n)");
+                repetir = char.Parse(Console.ReadLine());
+            } while (repetir == 's');
+
+        }
+    }
+}
