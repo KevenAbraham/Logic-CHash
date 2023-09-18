49b3c8b7ba603c1711eb0737b8d148c7077c5abb
KevenAbraham <abraham.keven20@gmail.com>
2023-09-18 09:45:53 -0300

    exercicio 1067 - numero impar até x

---
+++ exec1067/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace exec1067 {
+    internal class Program {
+        public static void Main(string[] args) {
+            int num;
+
+            Console.WriteLine("\nInforme o valor para ser ver os seus ímpares entre 1 e ele");
+            num = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= num; i++) {
+                if (i % 2 != 0) {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
