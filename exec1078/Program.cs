8ca161fe0444d482a1fe1b1474c3e958d5295378
KevenAbraham <abraham.keven20@gmail.com>
2023-09-18 08:55:13 -0300

    exercicio 1078 - tabuada

---
+++ exec1078/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace exec1078 {
+    internal class Program {
+        public static void Main(string[] args) {
+            int n;
+
+            Console.WriteLine("Insira o valor:");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++) {
+                int resultado = i * n;
+
+                Console.WriteLine($"{i} x {n} = {resultado}");
+            }
+
+        }
+    }
+}
