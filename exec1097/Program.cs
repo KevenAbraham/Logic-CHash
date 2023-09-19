513494c2a69d3928ab09706c02263b31c1b72c53
KevenAbraham <abraham.keven20@gmail.com>
2023-09-19 09:52:27 -0300

    exercicio 1097

--- exec1097/Program.cs
+++ exec1097/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace exec1097 {
+    internal class Program {
+        public static void Main(string[] args) {
+            int j = 7;
+            for (int i = 1; i <= 9; i = i + 2) {
+                Console.WriteLine("I=" + i + " J=" + j);
+                Console.WriteLine("I=" + i + " J=" + (j - 1));
+                Console.WriteLine("I=" + i + " J=" + (j - 2));
+                j = j + 2;
+            }
+        }
+    }
+}
