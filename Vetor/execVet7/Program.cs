c0dd8106233c290170ac8d73370ed209a0b74385
KevenAbraham <abraham.keven20@gmail.com>
2023-09-20 12:28:13 -0300

    exericicio 06 de vetor

---
+++ Vetor/execVet7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace execVet7 {
+    class Program {
+        static void Main(string[] args) {
+            int n, maiorIdade = 0;
+            string nomeMaiorIdade = "";
+
+            Console.Write("Informe a quantidade de pessoas: ");
+            n = int.Parse(Console.ReadLine());
+
+            string[] nome = new string[n];
+            int[] idade = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                Console.WriteLine($"Informe o nome da pessoa {i + 1}:");
+                nome[i] = Console.ReadLine();
+
+                Console.WriteLine($"Informe a idade da pessoa {i + 1}:");
+                idade[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < n; i++) {
+                if (idade[i] > maiorIdade) {
+                    maiorIdade = idade[i];
+                    nomeMaiorIdade = nome[i];
+                }
+            }
+
+            Console.WriteLine($"Pessoa mais velha: {nomeMaiorIdade}");
+        }
+    }
+}
