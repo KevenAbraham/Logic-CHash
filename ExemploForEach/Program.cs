1c2ac582715e06d0024ebf0113ac83228c1b8892
KevenAbraham <abraham.keven20@gmail.com>
2023-09-20 08:59:07 -0300

    exemplo de uso de vetores em c#

---
+++ ExemploForEach/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ExemploForEach {
+    class Program {
+        static void Main(string[] args) {
+            int n;
+
+            n = int.Parse(Console.ReadLine());
+            string[] vet = new string[n];
+
+            for (int i = 0; i < n; i++) {
+                vet[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("Nomes Lidos:");
+            foreach (string elemento in vet) { //elemento -> apelido do vetor
+                Console.WriteLine(elemento);
+            }
+
+        }
+    }
+}
