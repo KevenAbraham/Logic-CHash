695f2e9cd3e7625e0d3791cdeecbcadda4981225
KevenAbraham <abraham.keven20@gmail.com>
2023-09-20 10:38:20 -0300

    exercicio 05 de vetor

---
+++ Vetor/execVet5/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace execVet5 {
+    class Program {
+        static void Main(string[] args) {
+            int n; 
+            double media = 0, soma = 0;
+
+            Console.WriteLine("Informe a quantidade de valores para serem lidos: ");
+            n = int.Parse(Console.ReadLine());
+
+            double[] num = new double[n];
+                    
+            string[] vet = Console.ReadLine().Split(' ');
+
+            for(int i = 0; i < n; i++) {
+                num[i] = double.Parse(vet[i]);
+            }
+
+            for(int i = 0; i < n; i++) {
+                soma += num[i];
+            }
+
+            media = soma / n;
+
+            Console.WriteLine($"Média aritmética: {media:F3}");
+
+            Console.WriteLine("Elementos abaixo da média:");
+
+            for (int i = 0; i < n; i++) {
+                if (num[i] < media) {
+                    Console.WriteLine(num[i]);
+                }
+            }
+            
+        }
+    }
+}
