695dd4ab3d3f2b8094ef44fd72f60c5bfaf9df87
KevenAbraham <abraham.keven20@gmail.com>
2023-09-22 13:20:37 -0300

    exericcio 5 de matriz incompleto

---
+++ Matriz/execMat5/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace execMat5 {
+    class Program {
+        static void Main(string[] args) {
+            int n, m;
+
+            Console.Write("Informe o valor da linha: ");
+            m = int.Parse(Console.ReadLine());
+
+            Console.Write("Informe o valor da coluna: ");
+            n = int.Parse(Console.ReadLine());
+
+            int[,] matA = new int[m,n];
+            int[,] matB = new int[m,n];
+            
+        }
+    }
+}
