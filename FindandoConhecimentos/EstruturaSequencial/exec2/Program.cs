ac28a85881b398fd1c2b8614aa168aca94f3e864
KevenAbraham <abraham.keven20@gmail.com>
2023-09-25 09:47:38 -0300

    exercicio de fixacao 2 - estrutura sequencial

--- FindandoConhecimentos/EstruturaSequencial/exec2/Program.cs
+++ FindandoConhecimentos/EstruturaSequencial/exec2/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace exec2 {
+    class Program {
+        static void Main(string[] args) {
+            double raio, area;
+
+            Console.WriteLine("Informe o valor do raio do círculo: ");
+            raio = double.Parse(Console.ReadLine());
+
+            area = 3.14159 * (Math.Pow(raio,2));
+
+            Console.WriteLine($"A={area:F4}");
+        }
+    }
+}
