589865e39f8fb226c12f39ee1e0541eebfc31085
KevenAbraham <abraham.keven20@gmail.com>
2023-09-25 10:59:16 -0300

    finalizando os exercicios de fixação de conteúdo

--- FindandoConhecimentos/EstruturaRepetitiva/exec2/Program.cs
+++ FindandoConhecimentos/EstruturaRepetitiva/exec2/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace exec2 {
+    class Program {
+        static void Main(string[] args) {
+            int num, n, _in = 0, _out = 0; 
+
+            Console.WriteLine("Informe quantos números irá inserir: ");
+            n = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < n; i++) {
+                Console.WriteLine("\nInsira um número:");
+                num = int.Parse(Console.ReadLine());
+                if(num >= 10 && num <= 20) {
+                    _in++;
+                } else {
+                    _out++;
+                }
+            }
+
+            Console.WriteLine($"{_in} in");
+            Console.WriteLine($"{_out} out");
+        }
+    }
+}
